3418173cfd22447003064b3f32f98a1313395efb
juliofuentescerrada <julio.fuentes.cerrada@outlook.com>
2016-03-31 10:18:44 +0200

    A few optimizations

--- Jellyfish.MVC5.Minimal/global.asax.cs
+++ Jellyfish.MVC5.Minimal/global.asax.cs
@@ -9,6 +9,11 @@
     {
         protected void Application_Start(object sender, EventArgs e)
         {
+            MvcHandler.DisableMvcResponseHeader = true;
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             RouteTable.Routes.AppendTrailingSlash = true;
             RouteTable.Routes.LowercaseUrls = true;
             RouteTable.Routes.MapMvcAttributeRoutes();
