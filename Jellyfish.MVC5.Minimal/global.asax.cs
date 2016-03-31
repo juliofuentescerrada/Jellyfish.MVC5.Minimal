278247834c9a53a4a16fc98db7741511b4b02048
juliofuentescerrada <julio.fuentes.cerrada@outlook.com>
2016-03-31 13:40:15 +0200

    Added Autofac and AutoMapper packages

--- Jellyfish.MVC5.Minimal/global.asax.cs
+++ Jellyfish.MVC5.Minimal/global.asax.cs
@@ -1,22 +1,41 @@
 ﻿namespace Jellyfish.MVC5.Minimal
 {
     using System;
+    using System.Reflection;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
+
+    using Autofac;
+    using Autofac.Integration.Mvc;
 
     public class Global : HttpApplication
     {
         protected void Application_Start(object sender, EventArgs e)
         {
-            MvcHandler.DisableMvcResponseHeader = true;
+            AutofacConfig();
 
+            MvcHandler.DisableMvcResponseHeader = true;
             ViewEngines.Engines.Clear();
             ViewEngines.Engines.Add(new RazorViewEngine());
 
             RouteTable.Routes.AppendTrailingSlash = true;
             RouteTable.Routes.LowercaseUrls = true;
             RouteTable.Routes.MapMvcAttributeRoutes();
+        }
+
+        private static void AutofacConfig()
+        {
+            var builder = new ContainerBuilder();
+            
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinders(Assembly.GetExecutingAssembly());
+            builder.RegisterModelBinderProvider();
+            builder.RegisterModule<AutofacWebTypesModule>();
+            builder.RegisterSource(new ViewRegistrationSource());
+            builder.RegisterFilterProvider();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(builder.Build()));
         }
     }
 }
