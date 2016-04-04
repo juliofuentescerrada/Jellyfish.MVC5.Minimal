2c5116fd938ac5a24d7cdf477fcb5064241f1d90
juliofuentescerrada <julio.fuentes.cerrada@outlook.com>
2016-04-04 17:06:35 +0200

    blog template

---
+++ Jellyfish.MVC5.Minimal/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿namespace Jellyfish.MVC5.Minimal.Controllers
+{
+    using System.Web.Mvc;
+
+    public class BlogController : Controller
+    {
+        [Route("{year}/{month}/{day}/{post}")]
+        public ViewResult Post(int year, int month, int day, string post)
+        {
+            return this.View("post");
+        }
+    }
+}
